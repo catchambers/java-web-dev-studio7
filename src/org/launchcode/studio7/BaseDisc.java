6fc1d6c0c6b457051f4552ae18c2039259a6dfcf
CAT CHAMBERS <caitlynlchambers@gmail.com>
2020-09-14 19:22:14 -0500

    Adding BaseDisc abstract class code

--- src/org/launchcode/studio7/BaseDisc.java
+++ src/org/launchcode/studio7/BaseDisc.java
@@ -1,4 +1,39 @@
 package org.launchcode.studio7;
 
 public class BaseDisc {
+    private double storageCapacity;
+    private double storageUsed;
+    private String title;
+    private String contents;
+
+    public BaseDisc(double storageCapacity, double storageUsed, String title, String contents) {
+        this.storageCapacity = storageCapacity;
+        this.storageUsed = storageUsed;
+        this.title = title;
+        this.contents = contents;
+    }
+
+    public double getStorageCapacity() {
+        return storageCapacity;
+    }
+
+    public double getStorageUsed() {
+        return storageUsed;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getContents() {
+        return contents;
+    }
+
+    public void setStorageUsed(double storageUsed) {
+        this.storageUsed = storageUsed;
+    }
+
+    public void setContents(String contents) {
+        this.contents = contents;
+    }
 }
--- src/org/launchcode/studio7/OpticalDisc.java
+++ src/org/launchcode/studio7/OpticalDisc.java
@@ -3,6 +3,7 @@ package org.launchcode.studio7;
 public interface OpticalDisc {
     static final int centerDiameter = 15; //mm
     static final int discDiameter = 80; //mm
+    static final boolean isDataStorage = true;
 
     double storageAvailable(double storageCapacity, double storageInUse);
 
